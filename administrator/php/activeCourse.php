db8c4b328daa4cabfacb969abdd29ae7aedb116f
Zudwa-8 <34678214+Zudwa-8@users.noreply.github.com>
2023-01-17 17:49:46 +0500

    Delete administrator directory

--- administrator/php/activeCourse.php
+++
@@ -1,9 +0,0 @@
-<?
-    require_once($_SERVER['DOCUMENT_ROOT']."/php/cookieStart.php");
-    require_once($_SERVER['DOCUMENT_ROOT']."/php/connectDatabase.php");
-            
-    $query = "UPDATE `courses`
-    SET `active` = IF(`active` = 1, 0, 1)
-    WHERE `id_course` = ".$_POST['id_course'];
-    $mysql_result = mysqli_query($connectDatabase, $query);
-?>
