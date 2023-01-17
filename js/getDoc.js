605a85697fd9e71b5bcdea10e9d2a2a68c915743
Zudwa-8 <34678214+Zudwa-8@users.noreply.github.com>
2023-01-17 17:50:27 +0500

    Delete js directory

--- js/getDoc.js
+++
@@ -1,7 +0,0 @@
-function getDoc(id) {
-    var formData = new FormData();
-    formData.append("id", id);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/php/GetDoc.php", false);
-    xhr.send(formData);
-};
